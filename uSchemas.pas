a42c658e16efb77b83ba9875c511d1a8a374b1e9
jonat45 <jonatasdeaalves@gmail.com>
2018-02-21 00:33:05 -0300

    Create uSchemas.pas

---
+++ uSchemas.pas
@@ -0,0 +1,42 @@
+unit uSchemas ;
+
+interface
+uses
+     Classes,
+     Sysutils,
+     StrUtils,
+     DBClient,
+     DB ;
+
+type
+     TTBClientes = class( TClientDataSet )
+     public
+          ID_Cliente: TIntegerField ;
+          Codigo: TStringField ;
+          Nome: TStringField ;
+          
+          constructor create( AOwner: TComponent ) ; override ;
+     end ;
+     
+implementation
+
+constructro TTBClientes.create( AOwner: TComponent ) ;
+begin
+     inherited create( AOwner ) ;
+     FID_Cliente := TIntegerField.Create( self );
+     FID_Cliente.FieldName := 'ID_Cliente' ;
+     FID_Cliente.Required := false ;
+     FID_Cliente.DataSet := self ;
+
+     FCodigo := TStringField.Create( self ) ;
+     FCodigo.FieldName := 'Codigo' ;
+     FCodigo.Required := false ;
+     FCodigo.DataSet := self ;
+
+     FNome := TStringField.Create( self ) ;
+     FNome.FieldName := 'Nome' ;
+     FNome.Required := true ;
+     FNome.DataSet := self ;
+end ;
+
+end ;
